06072312ae63f0675a31b8667c6c091925a16b3c
Alê Borba <alexandre.borba@imasters.com.br>
2013-02-23 13:19:57 -0300

    Business::CPI::Gateway::Akatus created

--- lib/Business/CPI/Gateway/Akatus.pm
+++ lib/Business/CPI/Gateway/Akatus.pm
@@ -2,4 +2,16 @@ use strict;
 use warnings;
 package Business::CPI::Gateway::Akatus;
 
+#ABSTRACT: Business::CPI's Akatus driver
+
+use Moo;
+
+extends "Business::CPI::Gateway::Base";
+
+sub get_checkout_code{
+    die "Motherfucker";
+}
+
+
+
 1;
